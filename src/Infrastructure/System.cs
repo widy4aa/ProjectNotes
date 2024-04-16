ea428a74549932c7d175752e4e984b18e9edf301
degeneratezan <noxindocraft@gmail.com>
2024-04-16 10:41:29 +0700

    Menambahkan Class - Class

---
+++ src/Infrastructure/System.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectNotes.Base
+{
+    public class System
+    {
+        private static System _instance;
+        private readonly TaskQueue _taskQueue;
+        private readonly Database _database;
+
+        private System()
+        {
+            _taskQueue = new TaskQueue();
+            _database = new Database();
+        }
+
+        public static System Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new System();
+                }
+                return _instance;
+            }
+        }
+
+        public TaskQueue TaskQueue => _instance._taskQueue;
+        public Database Database => _instance._database;
+    }
+
+}
---
+++ src/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectNotes.Models
+{
+    internal class UserModel
+    {
+        private string _id;
+        public int Id { get; set; }
+    }
+}
